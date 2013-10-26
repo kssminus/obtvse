f628407e7c5c44e2e6660c7d4fcc7cb182091228
Seongsik,Kim <kssminus@gmail.com>
2013-10-26 11:01:57 +0900

    minor configuration change

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,31 +1,31 @@
-Sitemap::Generator.instance.load :host => "devopser.me" do
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://devopser.me"
 
-  # Sample path:
-  # path :posts
-  # The specified path must exist in your routes file (usually specified through :as).
-
-  # Sample path with params:
-  #   path :faq, :params => { :format => "html", :filter => "recent" }
-  # Depending on the route, the resolved url could be http://mywebsite.com/frequent-questions.html?filter=recent.
-
-  # Sample resource:
-  resources :posts, 
-            objects: proc {Post.published }, 
-            change_frequecy: "weekly",
-            skip_index: true,
-            skip_category: true
-
-
-  # Sample resource with custom objects:
-  #   resources :articles, :objects => proc { Article.published }
-
-  # Sample resource with search options:
-  #   resources :articles, :priority => 0.8, :change_frequency => "monthly"
-
-  # Sample resource with block options:
-  #   resources :activities,
-  #             :skip_index => true,
-  #             :updated_at => proc { |activity| activity.published_at.strftime("%Y-%m-%d") }
-  #             :params => { :subdomain => proc { |activity| activity.location } }
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+  Post.find_each do |p|
+    add p.slug, :lastmod => p.updated_at
+  end
 
 end
